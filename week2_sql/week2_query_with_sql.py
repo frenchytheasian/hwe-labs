efebe56d9eec6f06fc98e8bd64cd220d11664591
Michael French <michael.x.french@gmail.com>
2023-10-02 18:04:12 -0500

    week2

--- week2_sql/week2_query_with_sql.py
+++ week2_sql/week2_query_with_sql.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import lit
 
 ### Setup: Create a SparkSession
-spark = None
+spark = SparkSession.builder.appName("Week2").getOrCreate()
 
 # For Windows users, quiet errors about not being able to delete temporary directories which make your logs impossible to read...
 logger = spark.sparkContext._jvm.org.apache.log4j
@@ -11,35 +14,56 @@ logger.LogManager.getLogger("org.apache.spark.SparkEnv"). setLevel( logger.Level
 ### Questions
 
 # Question 1: Read the tab separated file named "resources/reviews.tsv.gz" into a dataframe. Call it "reviews".
+reviews = spark.read.csv("resources/reviews.tsv.gz", sep="\t", header=True)
 
 # Question 2: Create a virtual view on top of the reviews dataframe, so that we can query it with Spark SQL.
+reviews.createOrReplaceTempView("reviews")
 
 # Question 3: Add a column to the dataframe named "review_timestamp", representing the current time on your computer. 
+reviews = reviews.withColumn("review_timestamp", lit(datetime.now()))
 
 # Question 4: How many records are in the reviews dataframe? 
+print(reviews.count())
 
 # Question 5: Print the first 5 rows of the dataframe. 
 # Some of the columns are long - print the entire record, regardless of length.
+reviews.show(5, truncate=False)
 
 # Question 6: Create a new dataframe based on "reviews" with exactly 1 column: the value of the product category field.
 # Look at the first 50 rows of that dataframe. 
 # Which value appears to be the most common?
+product_category_df = reviews.select("product_category")
+product_category_df.show(50)
+# Digital_video_Games appears to be the most common
 
 # Question 7: Find the most helpful review in the dataframe - the one with the highest number of helpful votes.
 # What is the product title for that review? How many helpful votes did it have?
+reviews.createOrReplaceTempView("reviews")
+most_helpful_review = spark.sql("SELECT product_title, helpful_votes FROM reviews ORDER BY helpful_votes DESC LIMIT 1")
+most_helpful_review.show(truncate=False)
+# product_title: Xbox Live Subscription, helpful_votes: 993
 
 # Question 8: How many reviews exist in the dataframe with a 5 star rating?
+five_star = spark.sql("SELECT COUNT(*) FROM reviews WHERE star_rating = 5")
+five_star.show()
+# 80677
 
 # Question 9: Currently every field in the data file is interpreted as a string, but there are 3 that should really be numbers.
 # Create a new dataframe with just those 3 columns, except cast them as "int"s.
 # Look at 10 rows from this dataframe.
+int_cols = spark.sql("SELECT CAST(star_rating as INT) star_rating, CAST(helpful_votes as INT) helpful_votes, CAST(total_votes as INT) total_votes FROM reviews")
+int_cols.show(10)
 
 # Question 10: Find the date with the most purchases.
 # Print the date and total count of the date which had the most purchases.
+spark.sql("SELECT purchase_date, count(*) as count FROM reviews GROUP BY purchase_date ORDER BY purchase_date DESC LIMIT 1").show(truncate=False)
+# 2015-08-31, 108
 
 ##Question 11: Write the dataframe from Question 3 to your drive in JSON format.
 ##Feel free to pick any directory on your computer.
 ##Use overwrite mode.
+reviews.write.mode("overwrite").json("week2_sql/output")
 
 ### Teardown
 # Stop the SparkSession
+spark.stop()
